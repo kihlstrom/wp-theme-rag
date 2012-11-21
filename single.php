3d82e8c8f8398f90517a05c7adf38c5569d2ba8d
Mattias Kihlström <mattias@kihlstrom.com>
2012-11-21 17:03:42 +0100

    Styled single post heading.

--- single.php
+++ single.php
@@ -10,7 +10,6 @@
 		<div class="single">
 
 				<div class="heading">
-					<?php echo get_image( 'start_page_image' ); ?>
 					<h2><?php echo rag_title_split( get_the_title(), 24 ); ?></h2>
 					<div class="date"><span><?php the_date(); ?></span></div>
 				</div>
