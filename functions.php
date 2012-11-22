f200cecf175c77453739114f97e555b6825153cd
Mattias Kihlström <mattias@kihlstrom.com>
2012-11-22 16:01:39 +0100

    Breadcrumbs code moved to functions.php.

--- functions.php
+++ functions.php
@@ -9,6 +9,20 @@ function insert_jquery() {
    wp_enqueue_script( 'jquery' );
 }
 
+function rag_breadcrumbs( $post, $separator ) {
+	$id_parent = ( $post->post_parent ? $post->post_parent : $post->ID );
+	$nav_sub_list = wp_list_pages( 'sort_column=menu_order&depth=1&echo=0&title_li=&child_of=' . $id_parent );
+	$home_page = current( get_pages( 'sort_column=menu_order&depth=1&number=1' ) );
+	$html = '<a href="' . get_bloginfo( 'url' ) . '">' . $home_page->post_title . '</a>' . $separator;
+	if( $post->post_parent ) {
+		$parent_page = get_page( $post->post_parent );
+		$html = $html . '<a href="' . get_permalink( $post->post_parent ) . '">' . $parent_page->post_title . '</a>' . $separator;
+	}
+	$html = $html . '<span>' . $post->post_title . '</span>';
+	wp_reset_query();
+	return( $html );
+}
+
 function rag_title_split( $title, $max_characters_per_row ) {
 	$max_words_per_row = 4;
 	$title_word = explode( ' ', $title );
--- single.php
+++ single.php
@@ -2,18 +2,7 @@
 
 <div class="breadcrumbs">
 	<div class="inside">
-<?php
-$rag_parent_id = ( $post->post_parent ? $post->post_parent : $post->ID );
-$rag_nav_sub_list = wp_list_pages( 'sort_column=menu_order&depth=1&echo=0&title_li=&child_of='.$rag_parent_id );
-$rag_home_page = current( get_pages( 'sort_column=menu_order&depth=1&number=1' ) );
-
-echo '<a href="' . get_bloginfo('url') . '">' . $rag_home_page->post_title . '</a>/';
-if( $post->post_parent ) {
-	$parent_page = get_page( $post->post_parent );
-	echo '<a href="' . get_permalink( $post->post_parent ) . '">' . $parent_page->post_title . '</a>/';
-}
-echo '<span>' . $post->post_title . '</span>';
-?>
+		<?php echo rag_breadcrumbs( $post, ' &gt; ' ); ?>
 	</div>
 </div>
 
