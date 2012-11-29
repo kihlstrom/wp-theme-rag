24cc1d48e80f82cf04d27f91fd4d4785855cc2f6
Mattias Kihlström <mattias@kihlstrom.com>
2012-11-29 14:27:05 +0100

    Moved loading of main.js from head to end of body.

--- header.php
+++ header.php
@@ -8,7 +8,6 @@
 	<script>document.documentElement.className = document.documentElement.className.replace( 'noscript', 'script' );</script>
 	<link href='http://fonts.googleapis.com/css?family=Quicksand:300,400,700|Oswald:400,300,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>">
-	<script type="text/javascript" src="<?php echo THEME_DIR; ?>/js/main.js" ></script>	
 	<link rel="shortcut icon" href="<?php echo THEME_DIR; ?>/rag-fav-icon32x32.ico">
 	<?php wp_head(); ?>
 </head>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-﻿
+﻿var rag = window.rag || {};
+
+(function( me, $, undefined ) {
+
+	me.init = function() {
+//		alert('init');
+	}
+
+	return me;
+
+})( rag, jQuery );
+
+rag.init();
