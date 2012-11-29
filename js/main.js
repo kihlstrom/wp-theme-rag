b7b96effb47f9c945846f73e3c16dc33ea72fcf2
Mattias Kihlström <mattias@kihlstrom.com>
2012-11-29 23:25:45 +0100

    Post navigation expands on hover.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,19 @@
 
 (function( me, $, undefined ) {
 
+	function on_nav_post_mouseenter( event ) {
+		$( this ).stop( true ).animate( { width: '20em' }, 257, 'swing' );
+	}
+
+	function on_nav_post_mouseleave( event ) {
+		$( this ).stop( true ).animate( { width: '2em' }, 257, 'swing' );
+	}
+
 	me.init = function() {
-//		alert('init');
+		$( '.nav-post a' )
+			.on( 'mouseenter', on_nav_post_mouseenter )
+			.on( 'mouseleave', on_nav_post_mouseleave )
+		;
 	}
 
 	return me;
