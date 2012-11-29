a5c2cd46dbf5926e42bc910558f836b28d5b8cb3
Mattias Kihlström <mattias@kihlstrom.com>
2012-11-29 14:37:18 +0100

    Added missing load of main.js to footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 		<div class="inside">
 			<img src="<?php echo THEME_DIR; ?>/images/rag-by-mogul-and-addnode137x144.png" width="137" height="144" alt="Real Art Group by MOGUL">
 			<h3>Footer, the end of the page</h3>
-			<p>You have reached the footer. Not much going on around here at the moment, but you can always read about cookies or scroll <a href="#">back up</a> to where the magic happens. </p>
+			<p>You have reached the footer. Not much going on around here at the moment, but you can always read about cookies or scroll <a href="#">back up</a> to where the magic happens.</p>
 		</div>
 	</div>
 	<div class="footer-smallprint">
@@ -15,5 +15,6 @@
 </div>
 
 <?php wp_footer(); ?>
+<script type="text/javascript" src="<?php echo THEME_DIR; ?>/js/main.js"></script>
 </body>
 </html>
