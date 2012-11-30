21eff8bc4fe2c5556c692105ab9b9a97ed4d0736
Karl Löfholm <karl@lofholm@gmail.com>
2012-11-30 16:25:27 +0100

    Single case page

--- page_single_case_simple.php
+++ page_single_case_simple.php
@@ -9,31 +9,12 @@ get_header();
 	</div>
 </div>
 
-<div class="wrap">
+<div class="wrap simple_case">
 	<div class="inside">
+
+		<h2 class="headline"><?php the_title(); ?></h2>
+		<?php the_content(); ?>
 		
-        <?php the_content(); ?>
-
-        <div class="simple_case">
-            <h2 class="headline">Etiam id consequa arcu. venenatis</h2>
-            <img src="http://localhost:8888/Real-art-group/wp-content/uploads/2012/11/case_ring.png">
-
-            <div class="col-wrap col-two">
-				<div class="col right-shadow-wrap">
-					<div class="shadow top"></div>
-					<div class="shadow middle"></div>
-					<div class="shadow bottom"></div>
-				</div>
-				<div class="col">
-
-				</div>
-            </div>
-
-        </div>
-        
-
-
-
 	</div>
 </div>
 
